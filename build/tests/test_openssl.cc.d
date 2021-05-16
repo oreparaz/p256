c511b7e9737cf686f1ad24fee46ea42349d483c9
Oscar Reparaz <oreparaz@users.noreply.github.com>
2021-05-16 12:20:09 -0700

    delete checked-in build artifacts pre-gitignore modifications

--- build/tests/test_openssl.cc.d
+++
@@ -1,6 +0,0 @@
-build/tests/test_openssl.cc.o: tests/test_openssl.cc tests/../p256.h \
- tests/catch.hpp
-
-tests/../p256.h:
-
-tests/catch.hpp:
--- build/tests/unittest_main.cc.d
+++
@@ -1,3 +0,0 @@
-build/tests/unittest_main.cc.o: tests/unittest_main.cc tests/catch.hpp
-
-tests/catch.hpp:
